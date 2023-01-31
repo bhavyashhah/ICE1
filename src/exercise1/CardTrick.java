61dd5a777c47d34448a294979890616eade26115
Bhavya <shahbha@shernet.sheridancollege.ca>
2023-01-31 11:57:43 -0500

    Updated new

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -44,5 +44,27 @@ import java.util.Scanner;
               if(!found)
                   System.out.print("Card is not found in hand");
             }
+    private static void printInfo() {
     
+        System.out.println("Congratulations, you guessed right!");
+        System.out.println();
+        
+        System.out.println("My name is Saad, but you can call me prof, Paul or sir");
+        System.out.println();
+        
+        System.out.println("My career ambitions:");
+        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Have a semester with no violations of academic integrity!");
+	System.out.println();	
+
+        System.out.println("My hobbies:");
+        System.out.println("-- Investing");
+        System.out.println("-- Cooking");
+        System.out.println("-- Reading/Watching TV");
+        System.out.println("-- Riding my motorcycle");
+
+        System.out.println();
+    }
+
+
 }
